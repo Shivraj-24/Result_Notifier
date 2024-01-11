3da549dbc68dd73ce25929afe5e32280fcf1597b
Shivaraj A <109541049+Shivraj-24@users.noreply.github.com>
2024-01-11 16:53:25 +0530

    Initial Commit

---
+++ admin_sidebar.php
@@ -0,0 +1,34 @@
+<header class="header">
+		
+		<a href="">Admin Dashboard</a>
+
+		<div class="logout" style="margin-top: 15px;">
+			
+			<a href="index.html" class="btn btn-primary">Logout</a>
+
+		</div>
+
+	</header>
+
+
+	<aside>
+		
+		<ul>
+			
+			
+			<li>
+				<a href="add_student.php">Add Student</a>
+			</li>
+
+			<li>
+				<a href="view_student.php">View Student</a>
+			</li>
+
+			
+
+
+		</ul>
+
+
+	</aside>
+	
---
+++ adminhome.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Admin Dashboard</title>
+
+	<link rel="stylesheet" type="text/css" href="adminhome.css">
+
+	<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</head>
+<body>
+
+
+<header class="header">
+		
+		<a href="">Admin Dashboard</a>
+
+		<div class="logout" style="margin-top: 15px;">
+			
+			<a href="index.php" class="btn btn-primary">Logout</a>
+
+		</div>
+
+	</header>
+
+
+	<aside>
+		
+		<ul>
+			
+			
+			<li>
+				<a href="add_student.php">Add Student</a>
+			</li>
+
+			<li>
+				<a href="view_student.php">View Student</a>
+			</li>
+			
+
+			
+
+
+		</ul>
+
+
+	</aside>
+	<p style="margin-left: 150px; margin-top: 50px">
+		<b><h3 style="margin-left: 150px; margin-top: 50px">Welcome Admin, </h3></b>
+		<h5 style="margin-left: 150px; margin-top: 0px">Please select anyone option from sidebar</h5>
+	</p>
+
+</body>
+</html>
